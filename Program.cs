4fa41b64a7e41f23f5944259f201ea220dfcecca
vu giap <vudinhgiap11@gmail.com>
2023-07-11 08:47:05 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace chenphanntuvaomang
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("mời nhập vào phần tử n = ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("mời nhập vào giá tri của {0} = ", i);
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(" nhap gia tri can chen vao mang: ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(" moi nhap vao vi tri can chen: ");
+            int k = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(" mang truoc khi chen phan tu ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("{0} ", numbers[i]);
+            }
+            for (int i = n; i >= k; i--)
+            {
+                numbers[i] = numbers[i - 1];
+            }
+            Console.Write("mang sau khi chen phan tu: ");
+            for (int i = 0; i <= n; i++)
+            {
+                Console.Write("{0} ",numbers[i]);
+            }
+
+            Console.ReadKey();  
+        }
+    }
+}
